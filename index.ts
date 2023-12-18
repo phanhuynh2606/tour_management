aae8a9360bad8346455e67ee33ecc8d2b4607e79
Huynh Phan <huynhpvhe170150@fpt.edu.vn>
2023-12-18 22:00:14 +0700

    Tạo route cơ bản và cài đặt PUG

--- index.ts
+++ index.ts
@@ -3,11 +3,17 @@ import express,{Express,Request,Response} from "express"
 const app: Express = express();
 const port : Number = 3000;
 
+
+// Cài đặt PUG
+app.set("views","./views");
+app.set("view engine","pug");
+// Cài đặt PUG
+
 app.get("/",(req : Request,res:Response) =>{
     res.send("Trang Chu");
 });
 app.get("/tours",(req : Request,res:Response) =>{
-    res.send("Danh sách tour");
+    res.render("client/pages/tours/index");
 });
 
 app.listen(port,() =>{
