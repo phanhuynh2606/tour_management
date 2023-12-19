68d9fef892cebec62206f41a38c47fa5a3471111
Huynh Phan <huynhpvhe170150@fpt.edu.vn>
2023-12-20 01:00:30 +0700

    Change name folder controllers

--- routes/client/tour.route.ts
+++ routes/client/tour.route.ts
@@ -1,7 +1,7 @@
 import { Router } from "express";
 const router: Router = Router();
 
-import * as controller from "../../controller/client/tour.controller"
+import * as controller from "../../controllers/client/tour.controller"
 router.get("/", controller.list);
 
 export const tourRoutes: Router = router;
