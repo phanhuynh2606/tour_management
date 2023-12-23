84116c0168ecd0b8a9859178723e6f2c1cf18f92
Huynh Phan <huynhpvhe170150@fpt.edu.vn>
2023-12-23 15:29:41 +0700

    2.1. Lấy thông tin đơn hàng gửi lên server

--- public/js/cart.js
+++ public/js/cart.js
@@ -1,26 +1,28 @@
 // Vẽ ra danh sách tour
-const drawListTours = () =>{
-    fetch("/cart/list-json", {
-        method: "POST",
-        headers: {
-          "Content-type": "application/json",
-        },
-        body: localStorage.getItem("cart"),
-      })
-        .then((res) => res.json())
-        .then((data) => {
-          // console.log(data);
-          const htmlsArray = data.tours.map((item, index) => {
-            return `
+const drawListTours = () => {
+  fetch("/cart/list-json", {
+    method: "POST",
+    headers: {
+      "Content-type": "application/json",
+    },
+    body: localStorage.getItem("cart"),
+  })
+    .then((res) => res.json())
+    .then((data) => {
+      // console.log(data);
+      const htmlsArray = data.tours.map((item, index) => {
+        return `
               <tr>
                   <td>${index + 1}</td>
                   <td>
-                      <img src=${item.image} alt=${item.info.title} width="80px" />
+                      <img src=${item.image} alt=${
+          item.info.title
+        } width="80px" />
                   </td>
                   <td>
                       <a href='/tours/detail/${item.info.slug}'>${
-              item.info.title
-            }</a> 
+          item.info.title
+        }</a> 
                   </td>
                   <td>
                       ${item.price_special.toLocaleString()}
@@ -42,19 +44,19 @@ const drawListTours = () =>{
                   </td>
               </tr>
               `;
-          });
-          const listTour = document.querySelector("[list-tour]");
-          listTour.innerHTML = htmlsArray.join("");
-      
-          // Tính tổng đơn hàng
-          const totalPrice = data.tours.reduce((sum, item) => sum + item.total, 0);
-          const elementTotalPrice = document.querySelector("[total-price]");
-          elementTotalPrice.innerHTML = totalPrice.toLocaleString();
-          // Tính tổng đơn hàng
-          deleteItemInCart();
-          updateQuantityInCart();
-        });
-}
+      });
+      const listTour = document.querySelector("[list-tour]");
+      listTour.innerHTML = htmlsArray.join("");
+
+      // Tính tổng đơn hàng
+      const totalPrice = data.tours.reduce((sum, item) => sum + item.total, 0);
+      const elementTotalPrice = document.querySelector("[total-price]");
+      elementTotalPrice.innerHTML = totalPrice.toLocaleString();
+      // Tính tổng đơn hàng
+      deleteItemInCart();
+      updateQuantityInCart();
+    });
+};
 // Vẽ ra danh sách tour
 
 // Xoá sản phẩm trong giỏ hàng
@@ -62,13 +64,13 @@ const deleteItemInCart = () => {
   const listBtnDelete = document.querySelectorAll("[btn-delete]");
   listBtnDelete.forEach((button) => {
     button.addEventListener("click", () => {
-        const tourId = button.getAttribute("btn-delete");
-        console.log(tourId);
-        const cart = JSON.parse(localStorage.getItem("cart"));
+      const tourId = button.getAttribute("btn-delete");
+      console.log(tourId);
+      const cart = JSON.parse(localStorage.getItem("cart"));
 
-        const newCart = cart.filter(item => item.tourId != tourId);
-        localStorage.setItem("cart",JSON.stringify(newCart));
-        drawListTours();
+      const newCart = cart.filter((item) => item.tourId != tourId);
+      localStorage.setItem("cart", JSON.stringify(newCart));
+      drawListTours();
     });
   });
 };
@@ -76,17 +78,19 @@ const deleteItemInCart = () => {
 
 // cập nhật sản phẩm trong giỏ hàng
 const updateQuantityInCart = () => {
-  const listInputUpdate= document.querySelectorAll("[list-tour] input[item-id]");
+  const listInputUpdate = document.querySelectorAll(
+    "[list-tour] input[item-id]"
+  );
   listInputUpdate.forEach((input) => {
     input.addEventListener("change", () => {
-        const tourId = input.getAttribute("item-id");
-        const quantity = input.value;
-        const cart = JSON.parse(localStorage.getItem("cart"));
+      const tourId = input.getAttribute("item-id");
+      const quantity = input.value;
+      const cart = JSON.parse(localStorage.getItem("cart"));
 
-        const tourUpdate = cart.find(item => item.tourId == tourId);
-        tourUpdate.quantity = parseInt(quantity);
-        localStorage.setItem("cart",JSON.stringify(cart));
-        drawListTours();
+      const tourUpdate = cart.find((item) => item.tourId == tourId);
+      tourUpdate.quantity = parseInt(quantity);
+      localStorage.setItem("cart", JSON.stringify(cart));
+      drawListTours();
     });
   });
 };
@@ -95,3 +99,38 @@ const updateQuantityInCart = () => {
 // Lấy ra data và in ra giao diện
 drawListTours();
 // Hết Lấy ra data và in ra giao diện
+
+// Đặt tour
+const formOrder = document.querySelector("[form-order]");
+if (formOrder) {
+  formOrder.addEventListener("submit", (e) => {
+    e.preventDefault();
+    const fullName = e.target.elements.fullName.value;
+    const phone = e.target.elements.phone.value;
+    const note = e.target.elements.note.value;
+
+    const cart = JSON.parse(localStorage.getItem("cart"));
+    const data = {
+      info: {
+        fullName: fullName,
+        phone: phone,
+        note: note,
+      },
+      cart: cart,
+    };
+    console.log(data);
+    
+    fetch("/order", {
+      method: "POST",
+      headers: {
+        "Content-type": "application/json",
+      },
+      body: JSON.stringify(data),
+    })
+      .then((res) => res.json())
+      .then((data) => {
+        console.log(data);
+      });
+  });
+}
+// Hết đặt tour
--- routes/client/cart.route.ts
+++ routes/client/cart.route.ts
@@ -4,5 +4,6 @@ const router: Router = Router();
 import * as controller from "../../controllers/client/cart.controller"
 router.get("/", controller.index);
 router.post("/list-json", controller.listJson);
+router.post("/order", controller.listJson);
 
 export const cartRoutes: Router = router;
