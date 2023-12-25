787ef849cb5aefc7674b4f721b1aa897c89b4d33
Huynh Phan <huynhpvhe170150@fpt.edu.vn>
2023-12-26 05:54:43 +0700

    2.2. Server lấy data lưu vào bảng orders

--- controllers/client/order.controller.ts
+++ controllers/client/order.controller.ts
@@ -1,13 +1,40 @@
 import { Request, Response } from "express";
-
+import Order from "../../models/order.model";
+import { generateOrderCode } from "../../helpers/generate";
 
 // [POST] /admin/order
 export const order = async (req: Request, res: Response) => {
-    const data = req.body;
-    console.log(data);
+  const data = req.body;
 
-    res.json({
-        code: 200,
-        message : "Đặt hàng thành công"
-    })
-}
+  // Lưu data vào bảng orders
+  const dataOrder = {
+    code: "",
+    fullName: data.info.fullName,
+    phone: data.info.phone,
+    note: "",
+    status: "initial",
+  };
+
+  const order = await Order.create(dataOrder);
+
+  const orderId = order.dataValues.id;
+  const code = generateOrderCode(orderId);
+
+  await Order.update(
+    {
+      code: code,
+    },
+    {
+      where: {
+        id: orderId,
+      },
+    }
+  );
+   // Lưu data vào bảng orders_item
+
+  res.json({
+    code: 200,
+    message: "Đặt hàng thành công",
+    orderCode: code,
+  });
+};
